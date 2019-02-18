3429bd6dd76f797ba1b5b5bbfa8a4a0e66371a53
RajarshiBasu <rajobasu@Rajarshis-MacBook-Air.local>
2019-02-18 17:13:14 +0530

    template setup

--- USACO/GOLD_p1_2014.cpp
+++ USACO/GOLD_p1_2014.cpp
@@ -1,2 +1,26 @@
-#include <algorithm>
 #include <iostream>
+#include <vector>
+#include <set>
+#include <iomanip>
+#include <algorithm>
+#include <functional>
+#include <stdio.h>
+#include <cmath>
+#include <queue>
+#include <string>
+#include <map>
+#include <complex>
+#include <stack>
+#include <set>
+
+#define FOR(i,n) for(int i=0;i<n;i++)
+#define FORE(i,a,b) for(int i=a;i<=b;i++)
+#define ll long long int
+#define vi vector<int>
+#define ii pair<ll,ll>
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define pll pair<ll,ll>
+#define cd complex<double> 
