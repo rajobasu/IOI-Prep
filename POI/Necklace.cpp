64340aa72bae706ba42b944e5d2a80d0d50b3e32
RajarshiBasu <rajobasu@Rajarshis-MacBook-Air.local>
2019-04-13 12:48:00 +0530

    TODO:(obs made,not soln)

---
+++ POI/Necklace.cpp
@@ -0,0 +1,8 @@
+/*
+OBSERVATIONS: for each number, denote the first and last occurance of the number. Call this the range of the number.
+If we count the number of subarrays so that for all numbers that are present in the subarray, none of them have
+their range starting/ending outside, then we are done. This basically means that that number is present inside 
+our unit. also note that the sum of elements of such number should be equal to our size of subarray.
+
+
+*/
