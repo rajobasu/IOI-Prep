f194fbdebc5529878e24528c6db6dcce8bf7e718
RajarshiBasu <rajobasu@Rajarshis-MacBook-Air.local>
2019-04-14 14:59:26 +0530

    TODO:bnsrch,segtree,sparsetable

---
+++ POI/Frog.cpp
@@ -0,0 +1,4 @@
+/*
+SOLUTION : for each place, find the next place. This forms a directed graph with outdegree 1 for each node.
+Now, do a sparse table on this. (the first computation can be done via binsrch on segtree)
+*/
